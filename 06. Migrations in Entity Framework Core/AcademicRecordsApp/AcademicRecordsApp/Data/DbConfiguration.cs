6064fad61f872801eb3baaa94d013fcd39db59aa
Iliyana Dzhabirova <il.dzhabirova@abv.bg>
2025-03-20 15:48:19 +0200

    .

--- 06. Migrations in Entity Framework Core/AcademicRecordsApp/AcademicRecordsApp/Data/DbConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AcademicRecordsApp.Data
-{
-    public static class DbConfiguration
-    {
-        public const string ConnectionString =
-            @"Server=.;Database=AcademicRecordsDB;Trusted_Connection=True;Encrypt=False;";
-    }
-}
